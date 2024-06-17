b46d7a8c62307dacf35a33825955306b13db3bee
SantiagoCantero <santiagocanterotomas@gmail.com>
2024-06-17 10:04:08 -0300

    javascript

--- js/main.js
+++
@@ -1,9 +0,0 @@
-let nombre = prompt("ingresa tu nombre :");
-let altura = parseFloat (prompt("ingresa tu altura :"));
-let peso = parseFloat (prompt("ingresa tu peso :"));
-let altura_alcuadrado = altura*altura
-
-let mic = peso/altura_alcuadrado
-let salida = nombre + "tu mic es" + mic.toFixed (2)
-
-alert (salida)
