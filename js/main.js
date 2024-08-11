0469cfc2086c6249404f9abd7f33d07ea98c0ef6
SantiagoCantero <santiagocanterotomas@gmail.com>
2024-08-11 13:20:47 -0300

    entrega final

--- js/main.js
+++ js/main.js
@@ -1,187 +1,12 @@
-//PRODUCTOS
-const productos = [
-    //RAQUETAS HEAD
-    {
-        id: "Head Gravity PRO",
-        titulo: "Head Gravity PRO",
-        imagen: "./img/Head Gravity PRO.jpg",
-        categoria: {
-            nombre: "Head",
-            id: "head"
-        },
-        precio: 200000
-    },
-    {
-        id: "Head Prestige CLASSIC 2.0",
-        titulo: "Head Prestige CLASSIC 2.0",
-        imagen: "./img/Head Prestige CLASSIC 2.0.jpg",
-        categoria: {
-            nombre: "Head",
-            id: "head"
-        },
-        precio: 200000
-    },
-    {
-        id: "Head Prestige PRO",
-        titulo: "Head Prestige PRO",
-        imagen: "./img/Head Prestige PRO.jpg",
-        categoria: {
-            nombre: "Head",
-            id: "head"
-        },
-        precio: 200000
-    },
-    {
-        id: "Head Radical PRO",
-        titulo: "Head Radical PRO",
-        imagen: "./img/Head Radical PRO.jpg",
-        categoria: {
-            nombre: "Head",
-            id: "head"
-        },
-        precio: 200000
-    },
-    {
-        id: "Head Speed PRO Legend",
-        titulo: "Head Speed PRO Legend",
-        imagen: "./img/Head Speed PRO Legend.jpg",
-        categoria: {
-            nombre: "Head",
-            id: "head"
-        },
-        precio: 200000
-    },
-    {
-        id: "Head Speed PRO",
-        titulo: "Head Speed PRO",
-        imagen: "./img/Head Speed PRO.jpg",
-        categoria: {
-            nombre: "Head",
-            id: "head"
-        },
-        precio: 200000
-    },   
-    {
-        id: "Wilson Blade 16x20 V9",
-        titulo: "Wilson Blade 16x20 V9",
-        imagen: "./img/Wilson Blade 16x20 V9.jpg",
-        categoria: {
-            nombre: "Wilson",
-            id: "wilson"
-        },
-        precio: 200000
-    },
-    {
-        id: "Wilson Blade 98 16x19 V9",
-        titulo: "Wilson Blade 98 16x19 V9",
-        imagen: "./img/Wilson Blade 98 16x19 V9.jpg",
-        categoria: {
-            nombre: "Wilson",
-            id: "wilson"
-        },
-        precio: 200000
-    },
-    {
-        id: "Wilson Burn 100 V4",
-        titulo: "Wilson Burn 100 V4",
-        imagen: "./img/Wilson Burn 100 V4.jpg",
-        categoria: {
-            nombre: "Wilson",
-            id: "wilson"
-        },
-        precio: 200000
-    },
-    {
-        id: "Wilson Burn 100 V5.0",
-        titulo: "Wilson Burn 100 V5.0",
-        imagen: "./img/Wilson Burn 100 V5.0.jpg",
-        categoria: {
-            nombre: "Wilson",
-            id: "wilson"
-        },
-        precio: 200000
-    },
-    {
-        id: "Wilson Pro Staff 97 V14",
-        titulo: "Wilson Pro Staff 97 V14",
-        imagen: "./img/Wilson Pro Staff 97 V14.jpg",
-        categoria: {
-            nombre: "Wilson",
-            id: "wilson"
-        },
-        precio: 200000
-    },
-    {
-        id: "Wilson Ultra 100L V4.0",
-        titulo: "Wilson Ultra 100L V4.0",
-        imagen: "./img/Wilson Ultra 100L V4.0.jpg",
-        categoria: {
-            nombre: "Wilson",
-            id: "wilson"
-        },
-        precio: 200000
-    },
-    {
-        id: "Yonex EZONE 98",
-        titulo: "Yonex EZONE 98",
-        imagen: "./img/Yonex EZONE 98.jpg",
-        categoria: {
-            nombre: "Yonex",
-            id: "yonex"
-        },
-        precio: 200000
-    },
-    {
-        id: "Yonex EZONE 98L",
-        titulo: "Yonex EZONE 98L",
-        imagen: "./img/Yonex EZONE 98L.jpg",
-        categoria: {
-            nombre: "Yonex",
-            id: "yonex"
-        },
-        precio: 200000
-    },
-    {
-        id: "Yonex PERCEPT 97",
-        titulo: "Yonex PERCEPT 97",
-        imagen: "./img/Yonex PERCEPT 97.jpg",
-        categoria: {
-            nombre: "Yonex",
-            id: "yonex"
-        },
-        precio: 200000
-    },
-    {
-        id: "Yonex PERCEPT 97D",
-        titulo: "Yonex PERCEPT 97D",
-        imagen: "./img/Yonex PERCEPT 97D.jpg",
-        categoria: {
-            nombre: "Yonex",
-            id: "yonex"
-        },
-        precio: 200000
-    },
-    {
-        id: "Yonex VCORE 95",
-        titulo: "Yonex VCORE 95",
-        imagen: "./img/Yonex VCORE 95.jpg",
-        categoria: {
-            nombre: "Yonex",
-            id: "yonex"
-        },
-        precio: 200000
-    },
-    {
-        id: "Yonex VCORE 98",
-        titulo: "Yonex VCORE 98",
-        imagen: "./img/Yonex VCORE 98.jpg",
-        categoria: {
-            nombre: "Yonex",
-            id: "yonex"
-        },
-        precio: 200000
-    }
-];
+let productos = [];
+
+fetch("./js/productos.json")
+    .then(response => response.json())
+    .then(data => {
+        productos = data;
+        cargarProductos(productos);
+    })
+
 
 const contenedorProductos = document.querySelector("#contenedor-productos");
 const botonesCategorias = document.querySelectorAll(".boton-categoria");
@@ -212,8 +37,6 @@ function cargarProductos(productosElegidos) {
 
     actualizarBotonesAgregar();
 }
-
-cargarProductos(productos);
 
 botonesCategorias.forEach (boton => {
     boton.addEventListener("click", (e) => {
@@ -253,6 +76,25 @@ if (productosEnCarritoLS) {
 }
 
 function agregarAlCarrito(e) {
+    Toastify({
+        text: "Producto Agregado",
+        duration: 3000,
+        close: true,
+        gravity: "top", //`top` or `bottom`
+        position: "right", //`left`, `center` or `right`
+        stopOnFocus: true, //Prevents dismissing of toast on hover
+        style: {
+            background: "linear-gradient(to right, #046A3A, #7B65A2)",
+            borderRadius: "2rem",
+            textTransform: "uppercase",
+            fontSize: "1rem"
+        },
+        offset: {
+            x: '1.5rem', //horizontal axis - can be a number or a string indicating unity. eg: '2em'
+            y: '1.5rem', //vertical axis - can be a number or a string indicating unity. eg: '2em'
+        },
+        onClick: function(){} //Callback after click
+    }).showToast();
 
     const idBoton = e.currentTarget.id;
     const productoAgregado = productos.find(producto => producto.id === idBoton);
